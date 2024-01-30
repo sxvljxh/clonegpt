75939bd1ea4eebd3ac16c618191895f5917a637e
sxvl <47700416+sxvljxh@users.noreply.github.com>
2024-01-30 15:41:45 +0000

    added google auth using next-auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col h-screen p-2">
       <div className="flex-1">
         <div>
           <NewChat />
 
-          <div>
-            {/* model selection */}
-          </div>
+          <div>{/* model selection */}</div>
 
           {/* map through chat rows */}
         </div>
       </div>
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="Profile Picture"
+          className="h-12 w-12 rounded-full cursor-pointer mb-2 mx-auto hover:opacity-50"
+          onClick={() => signOut()}
+        />
+      )}
     </div>
   );
 }
