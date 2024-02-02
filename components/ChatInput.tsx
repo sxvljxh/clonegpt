0168e5c5273b63bad46d0afab5be46d0ef20b859
sxvl <47700416+sxvljxh@users.noreply.github.com>
2024-02-02 18:03:56 +0000

    chat page: prompt chatgpt; persist prompt to firebase through client, persist completion to firebase through firebase admin

---
+++ components/ChatInput.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { db } from "@/firebase";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { FormEvent, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+type Props = {
+  chatId: string;
+};
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  // TODO: useSWR to get model
+  const model = "gpt-3.5-turbo-instruct";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image ||
+          `https://ui-avatars.com/api/?name='${session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    // toast notification to say loading
+    const notification = toast.loading("ChatGPT is thinking...");
+
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(() => {
+      // TODO: toast notification to say successful
+      toast.success("ChatGPT has responded", {
+        id: notification,
+      });
+    });
+  };
+
+  return (
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+      <form className="p-5 space-x-5 flex" onSubmit={sendMessage}>
+        <input
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
+          type="text"
+          placeholder="Type your message here..."
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          disabled={!prompt || !session}
+          className="bg-[#11a37f] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+        >
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatInput;
