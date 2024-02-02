f64b676a7d60e15b1faba59c9f2c64ef5d0424f6
sxvl <47700416+sxvljxh@users.noreply.github.com>
2024-02-02 18:05:42 +0000

    sidebar: chat row integration with firebase + chatgpt

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-
+import { useCollection } from "react-firebase-hooks/firestore";
 import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import ChatRow from "./ChatRow";
 
 function Sidebar() {
   const { data: session } = useSession();
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  // console.log(chats);
 
   return (
     <div className="flex flex-col h-screen p-2">
@@ -16,6 +28,9 @@ function Sidebar() {
           <div>{/* model selection */}</div>
 
           {/* map through chat rows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
       {session && (
